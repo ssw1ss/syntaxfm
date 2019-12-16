669f8c16b5075f86816a98859bbd256c27cd4903
Lukas Hostettler <Lukas@mac.local>
2019-12-16 13:44:03 -0500

    added gatsby-plugin-netlify-cache

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,7 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    `gatsby-plugin-netlify-cache`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
